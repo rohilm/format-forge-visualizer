a983deb759cae714e4a4e0554094c0c6b8befaca
rohilm <103750236+rohilm@users.noreply.github.com>
2025-09-24 17:56:32 +0530

    Update supabaseService.ts

--- src/services/supabaseService.ts
+++ src/services/supabaseService.ts
@@ -1,7 +1,7 @@
 import { supabase } from '@/lib/supabase';
 import { Template, FormField, FormData, FormSubmission } from '@/types';
 import { convertFieldPositionsToPercentages, convertFieldPositionsToPixels, PercentagePosition } from '@/utils/positionUtils';
-// this is inserted to test
+// below line is kept for test
 import { toast } from "@/components/ui/use-toast";
 
 
